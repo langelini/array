00e8d6f47a73d92005f472db2b7ce66c51a53831
langelini <leif_angelini27@milton.edu>
2025-02-21 13:01:57 -0500

    h

--- src/makingarrays.java
+++ src/makingarrays.java
@@ -1,5 +1,5 @@
 public class makingarrays {
-    int [] nums = new int[3];
+    int [] nums = new int[10];
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
@@ -13,7 +13,15 @@ public class makingarrays {
         nums[1] = 5;
         nums[0] = 10;
         nums[2] = 15                                                                                                                                               ;
-        nums [ 2 ] = nums[1]*3                                                                                                                                              ;
+        nums [ 2 ] = nums[1]*3;
+        nums  [3] = 20;
+        nums  [4] = 25;
+        nums  [5] = 30;
+        nums  [6] = 35;
+        nums  [7] = 40;
+        nums  [8] = 45;
+        nums  [9] = 50;
+
         printarray();
     }
     public void printarray(){
