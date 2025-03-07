314b12c9041a7a3daf2a1f8ee1dd05ee1f92a504
langelini <leif_angelini27@milton.edu>
2025-03-07 13:28:01 -0500

    h

--- src/arraysobject.java
+++ src/arraysobject.java
@@ -1,6 +1,6 @@
 public class arraysobject {
     public static void main(String[] args) {
-
+            arraysobject a = new arraysobject();
     }
     burger[] burg = new burger [10];
     public arraysobject (){
@@ -10,23 +10,23 @@ public class arraysobject {
             burg[x] = innout;
         }
         changesizes();
-        printArray();
         totalsushi();
+        printArray();
     }
     public void printArray(){
-        for (burger burger : burg) {
-            burger.printinfo();
+        for (int x = 0;x<burg.length;x++) {
+            burg[x].printinfo();
         }
     }
 public void changesizes(){
-    for (burger burger : burg) {
-        burger.size = (int) (Math.random() * 10) + 10;
+    for (int x = 0;x<burg.length;x++) {
+        burg[x].size = (int) (Math.random() * 10) + 10;
     }
 }
 public void totalsushi() {
         int totalsum = 0;
-    for (burger burger : burg) {
-        totalsum = totalsum + burger.size;
+    for (int x = 0;x<burg.length;x++) {
+        totalsum = totalsum + burg[x].size;
     }
     System.out.println("total size sum: "+totalsum);
 }
